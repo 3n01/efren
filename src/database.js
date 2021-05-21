d94671a11c341fac3a3842981d05fcfdee1cefa0
Enol Cachero Ramírez <enol@23xy.enol.com>
2021-05-21 14:52:30 +0200

    Fix database

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const URI = 'mongodb://enolc:enolc@localhost:27017/admin';
 const URI_LOCAL = "mongodb://localhost:27017/admin"
 
-mongoose.connect(URI_LOCAL).then(
+mongoose.connect(URI).then(
     (result) => console.log(`Success connecting to DB -> ${Object.keys(result.models)}`),
     (err) => console.log(`Error connecting to DB -> ${err}`)
 )
