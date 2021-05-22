bd66dd88482d0e4902d4f5da3cbf60f3ca18f118
Enol Cachero Ramírez <enol@23xy.enol.com>
2021-05-22 17:02:30 +0200

    Se habilita database.js

---
+++ src/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const URI = 'mongodb://enolc:enolc@localhost:27017/admin';
+const URI_LOCAL = "mongodb://localhost:27017/admin"
+
+mongoose.connect(URI).then(
+    (result) => console.log(`Success connecting to DB -> ${Object.keys(result.models)}`),
+    (err) => console.log(`Error connecting to DB -> ${err}`)
+)
+
+
+module.exports = mongoose;
